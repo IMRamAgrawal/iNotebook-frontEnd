643a7e39f28d80141045c998f719d32fadfdef2f
IMRamAgrawal <123664629+IMRamAgrawal@users.noreply.github.com>
2023-05-06 03:38:17 -0700

    second  commits

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://imramagrawal:Jaimongodb123@cluster0.r7lshib.mongodb.net/notebooks?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://imramagrawal:jaimongodb123@cluster0.s07gac8.mongodb.net/inotebook?retryWrites=true&w=majority")
 .then(()=> {console.log("connection successfull...")})
 .catch((err)=> {console.log(err)});
